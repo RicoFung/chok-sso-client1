ad16854be83f9d1145fb253e3917215e54ed38d8
ricofung <ricofungcn@icloud.com>
2018-09-25 11:07:14 +0800

    初步实现/oauth/logout

--- src/main/java/com/webconfig/WebSecurityConfig.java
+++ src/main/java/com/webconfig/WebSecurityConfig.java
@@ -12,6 +12,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception
 	{
-		http.antMatcher("/**").authorizeRequests().anyRequest().authenticated();
+		http.antMatcher("/**").authorizeRequests().anyRequest().authenticated()
+		.and()
+		.logout()
+        .logoutSuccessUrl("http://localhost:9090/chok-sso/exit");
 	}
 }
