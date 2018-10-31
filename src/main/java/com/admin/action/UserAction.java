541a8dcefe0fad7cb0d4aa1f443af78a34294931
ricofung <ricofungcn@icloud.com>
2018-10-31 15:41:01 +0800

    fix pom.xml

--- src/main/java/com/admin/action/UserAction.java
+++ src/main/java/com/admin/action/UserAction.java
@@ -2,16 +2,20 @@ package com.admin.action;
 
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserAction
 {
+	static Logger log = LoggerFactory.getLogger(UserAction.class);
+	
 	@RequestMapping("/user/me")
 	public Principal user(Principal principal)
 	{
-		System.out.println(principal);
+		log.info(principal+"");
 		return principal;
 	}
 }
