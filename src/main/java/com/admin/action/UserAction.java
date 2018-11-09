9ba5e92ab5d3e689a1e82c85ba627eed2c385b63
ricofung <ricofungcn@icloud.com>
2018-11-09 16:20:03 +0800

    fix action

--- src/main/java/com/admin/action/UserAction.java
+++ src/main/java/com/admin/action/UserAction.java
@@ -1,21 +1,35 @@
 package com.admin.action;
 
 import java.security.Principal;
+import java.util.Collection;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserAction
 {
 	static Logger log = LoggerFactory.getLogger(UserAction.class);
-	
-	@RequestMapping("/user/me")
-	public Principal user(Principal principal)
+
+	@RequestMapping("/me")
+	public Principal me(Principal principal)
 	{
-		log.info(principal+"");
+		log.info(principal + "");
 		return principal;
+	}
+
+	@RequestMapping("/roles")
+	@ResponseBody
+	public Collection<? extends GrantedAuthority> roles()
+	{
+		Collection<? extends GrantedAuthority> roles = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		log.info(roles + "");
+		return roles;
 	}
 }
